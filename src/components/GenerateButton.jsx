32a57ac817d43bbfc2a0a899c430d5bf3d737b77
Samwel Charles <younggeeks101@gmail.com>
2019-02-17 23:18:24 +0300

    add generatebutton.jsx component

---
+++ src/components/GenerateButton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Dropdown } from "semantic-ui-react";
+import { countryOptions } from "../helpers/common.js";
+
+class GenerateButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "ke"
+    };
+  }
+  handleChange = (e,{ value }) => {
+    console.log("the value is ", value)
+    this.setState({ value });
+  }
+  render() {
+    const {value} = this.state;
+    const {onGenerate} = this.props;
+    return (
+      <div>
+        <Dropdown
+          onChange={this.handleChange}
+          value={value}
+          placeholder="Select Country"
+          fluid
+          search
+          selection
+          options={countryOptions}
+        />
+        <Button onClick={()=>onGenerate(value)}>Generate</Button>
+      </div>
+    );
+  }
+}
+
+export default GenerateButton;
