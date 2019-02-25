75a4b901da97e1230035c35575b2cda325ddbedc
Samwel Charles <younggeeks101@gmail.com>
2019-02-26 00:11:35 +0300

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {create} from 'react-test-renderer';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('contains the tags as defined', () => {
+  const appComponent = create(
+    <App/>,
+  );
+  let tree = appComponent.toJSON()
+  expect(tree).toMatchSnapshot()
+});
+
+it('contains correct length of numbers array', () => {
+
+  const appComponent = create(
+    <App/>,
+  );
+  const instance = appComponent.getInstance()
+  expect(instance.state.numbers.length).toEqual(10000)
+});
+it('Generates numbers array', () => {
+
+  const appComponent = create(
+    <App/>,
+  );
+  const instance = appComponent.getInstance()
+
+  const {max,min} = instance.state
+    console.log("the max ", max,min)
+  expect(instance.state.numbers.length).toEqual(10000)
+  instance.generate()
+  expect(max===instance.state.max).toBeFalsy()
+});
+it('Changes direction when sort is called', () => {
+  const appComponent = create(
+    <App/>,
+  );
+  const instance = appComponent.getInstance()
+  expect(instance.state.numbers.length).toEqual(10000)
+  instance.sort()
+  expect(instance.state.direction).toEqual("descending")
+});
+
--- src/helpers/numbers.js
+++
@@ -1,1 +0,0 @@
-export const randomNumber =() => Math.floor(Math.random() * 9000000000) + 2000000000;
